d7c4b10f7e562c17e6b167ff0b186485e48cbd73
Timoteo Dima <timoteodp10@gmail.com>
2020-09-19 17:46:03 -0300

    Adicionando frontend-web

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello word Tim is programming.
+         Hello world, Tim is programming
         </p>
         <a
           className="App-link"
